26ffdac6f3cd9605d2b0b6b93739b48a8b239750
Daram Guru <daramgurum33@gmail.com>
2024-04-30 21:35:32 +0530

    React Router Configured

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
-
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Error from "./Components/Error";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 //Planning the Project
 /**
  * Header
@@ -27,5 +30,23 @@ const AppLayout = () => {
     </>
   );
 };
+
+console.log(1 + "1" - 1);
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root2"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/Components/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <>
+      <h1>
+        Hey This is <span style={{ color: "blue" }}>Contact</span> Page
+      </h1>
+    </>
+  );
+};
+
+export default Contact;
